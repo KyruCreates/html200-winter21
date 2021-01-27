1c2a58095195116aaadeeb92a041b45dd9fb1db0
KyruCreates <17752737+KyruCreates@users.noreply.github.com>
2021-01-26 23:22:59 -0800

    Lesson 02 Assignment 1

---
+++ calculator.js
@@ -0,0 +1,36 @@
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+
+console.log(a + b);
+console.log(c - d);
+console.log(b * d);
+console.log(c / a);
+
+// a more advanced variant
+// let result;
+// const operator = prompt('Enter operator (either +, -, *, or /): ');
+// const num1 = parseFloat(prompt('Enter first number: '));
+// const num2 = parseFloat(prompt('Enter second number: '));
+// switch (operator) {
+//   case '+':
+//     result = num1 + num2;
+//     console.log('${num1} + ${num2} = ${result}');
+//     break;
+//   case '-':
+//     result = num1 - num2;
+//     console.log('${num1} - ${num2} = ${result}');
+//     break;
+//   case '*':
+//     result = num1 * num2;
+//     console.log('${num1} * ${num2} = ${result}');
+//     break;
+//   case '/':
+//     result = num1 / num2;
+//     console.log('${num1} * ${num2} = ${result}');
+//     break;
+//   default:
+//     console.log('Invalid');
+//     break;
+// }
