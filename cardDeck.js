8ce8c3a1bc2b5766777f872a8fc8288857d68e61
KyruCreates <17752737+KyruCreates@users.noreply.github.com>
2021-01-27 03:03:03 -0800

    Lesson 02 Assignment 5 and changed countdown to use let instead of var

---
+++ cardDeck.js
@@ -0,0 +1,28 @@
+// creates a standard 52-card deck
+function deckBuilder() {
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", ];
+  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  const cards = [];
+
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      const value = values[j];
+      const suit = suits[i];
+      // pushes combined data into the cards array
+      cards.push({value, suit});
+    }
+  }
+  return cards;
+}
+
+const cards = deckBuilder();
+randomCard(cards);
+
+// choose a random card from the cards array
+function randomCard(cards) {
+  const random = Math.floor(Math.random() * 51);
+  const cardValue = cards[random].value;
+  const cardSuit = cards[random].suit;
+  // display the random card value and card suit
+  console.log(cardValue + " of " + cardSuit);
+}
