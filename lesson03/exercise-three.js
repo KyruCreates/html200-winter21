d2c7e023eae5a4e39de2061440d4f5053136259c
KyruCreates <17752737+KyruCreates@users.noreply.github.com>
2021-02-03 20:05:03 -0800

    exercise-three.js finished

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,6 @@
+// Create an array that has at least 5 strings
+// Use a for loop to console log all of the elements plus the index and a comma
+let arr = ['aglio e olio', 'bolognese', 'spaghetti', 'ravioli', 'fettuccine alfredo'];
+for(let i = 0; i < arr.length; i++) {
+  console.log(arr.indexOf(arr[i]) + ', ' + arr[i]);
+}
