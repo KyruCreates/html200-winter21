54383d6143001a30d31386af43975afcd2084524
KyruCreates <17752737+KyruCreates@users.noreply.github.com>
2021-02-03 19:49:55 -0800

    added exercise-two.js and created files for exercise-three.js and exercise-five.js

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,5 @@
+// Set a string variable to be a sentence of at least five words and a period.
+// Make an array from that sentence and console.log the third word.
+let str = 'This is a sentence about pasta.';
+let arr = str.split(' ');
+console.log(arr[2]);
